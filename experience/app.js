9a5cec30ba587720a5087e7093f4ad41debd0d99
Elmir22 <elmirabbasov002@gmail.com>
2022-07-02 20:38:55 +0000

    js

--- experience/app.js
+++ experience/app.js
@@ -1,4 +1,31 @@
 "use strict";
 $(document).ready(function () {
 
+
+    // $('h1').click(function (e) { 
+    //     e.preventDefault();
+    //     $('.btn').html('necesen');
+    // });
+
+
+    $('.btn').click(function (e) { 
+        e.preventDefault();
+        
+        let a = a => {
+            for (let i = 0; i <= a; i++) {
+                $('.col-md-12').append(`<h1>${i}</h1>`);
+                
+            }
+        }
+        a($('input').val())
+    });
+
+
+
+
+
+
+
+
+
 });
