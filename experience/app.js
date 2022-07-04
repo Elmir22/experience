323fe45165d590de9bdde20e22b26ef57b9bcb03
Elmir22 <elmirabbasov002@gmail.com>
2022-07-04 21:38:37 +0000

    js new

--- experience/app.js
+++ experience/app.js
@@ -8,16 +8,27 @@ $(document).ready(function () {
     // });
 
 
-    $('.btn').click(function (e) { 
+    // $('.btn').click(function (e) { 
+    //     e.preventDefault();
+
+    //     let a = a => {
+    //         for (let i = 0; i <= a; i++) {
+    //             $('.col-md-12').append(`<h1>${i}</h1>`);
+
+    //         }
+    //     }
+    //     a($('input').val())
+    // });
+
+    $('.btn').click(function (e) {
         e.preventDefault();
-        
-        let a = a => {
-            for (let i = 0; i <= a; i++) {
-                $('.col-md-12').append(`<h1>${i}</h1>`);
+        let k = v => {for (let i = 0; i<v; i++) 
+            { $('.box').append(`<h3>${v}</h3>`)}
                 
             }
-        }
-        a($('input').val())
+        let z = e => Number(e) ? k(e) : $('.box').append(`<h3>${e}</h3>`)
+            
+        z($('input').val())
     });
 
 
