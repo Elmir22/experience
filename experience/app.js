8227fd7b390e2ab8d633958d4ee8a9d6f1f63646
Elmir22 <elmirabbasov002@gmail.com>
2022-07-08 17:29:05 +0000

    js

--- experience/app.js
+++ experience/app.js
@@ -22,12 +22,11 @@ $(document).ready(function () {
 
     $('.btn').click(function (e) {
         e.preventDefault();
-        let k = v => {for (let i = 0; i<v; i++) 
-            { $('.box').append(`<h3>${v}</h3>`)}
-                
-            }
+        let k = v => {
+            for (let i = 0; i < v; i++) { $('.box').append(`<h3>${v}</h3>`) };
+        }
         let z = e => Number(e) ? k(e) : $('.box').append(`<h3>${e}</h3>`)
-            
+        ;
         z($('input').val())
     });
 
